3c8bf77dd05dcd20b2fc4889f47bafc5791f4611
Jon Washington <jonathan.washington@gmail.com>
2018-11-13 20:48:05 -0800

    added javascript to render the iframe, hopefully to make the process of updating videos at a button click much easier

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,37 @@
 function pandaServe(){
-  
+
+}
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady(){
+  player = new YT.Player('player',{
+    height:'390',
+    width:'640',
+    videoId:'iVXtm-S2GbI',
+    events:{
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event){
+  event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event){
+  if(event.data == YT.playerState.PLAYING && !done){
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo(){
+  player.stopVideo();
 }
